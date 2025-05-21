2f1121dd5583d5553e63946383f33774a2c86c3f
Rushang Chauhan <rushang.chauhan@bolt.eu>
2025-05-21 17:29:24 +0300

    updated code

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import Fastify from "fastify";
-const { App, say } = require("@slack/bolt");
+const { App, say } = require('@slack/bolt');
+import { searchXmlItems } from "./source";
 
 // Initializes your app with your bot token and signing secret
 const slackApp = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+
 (async () => {
-    // Start your app
-    await slackApp.start(4000);
-    slackApp.logger.info("⚡️ Bolt app is running!");
+  // Start your app
+  await slackApp.start(4000);
+  slackApp.logger.info('⚡️ Bolt app is running!');
 })();
 
 const server = Fastify();
@@ -20,26 +22,31 @@ server.get("/ping", async (request, reply) => {
 });
 
 server.post("/submit", async (request: any, reply) => {
-    console.log("🚀 ~ server.post ~ request:", request);
-    const event = request.body.event;
-    const userId: string = event.user
-    const rawText: string = event.text;
-    const cleanText = rawText.replace("<@U08TFACE9TN>", "");
-
-    sendMessage(YOUR_CHANNEL_ID, { userId, cleanText });
+  console.log("🚀 ~ server.post ~ request:", request);
+  const event = request.body.event;
+  const userId: string = event.user
+  const rawText: string = event.text;
+  const cleanText = rawText.replace("<@U08TFACE9TN>", "");
+  try {
+    const results = await searchXmlItems("src/SearchRequest.xml", cleanText);
+    sendMessage(YOUR_CHANNEL_ID, { userId, results });
+  } catch (error) {
+    console.error("Search error:", error);
+  }
 });
 
+
 async function sendMessage(channelId: string, data: any ) {
-    try {
-        const message = `<@${data?.userId}> ${data?.cleanText}`;
-        await slackApp.client.chat.postMessage({
-            channel: channelId,
-            text: message,
-        });
-        console.log(`Message sent to channel ${channelId}`);
-    } catch (error) {
-        console.error("Error sending message:", error);
-    }
+  try {
+      const message = `<@${data?.userId}> ${data?.results}`;
+      await slackApp.client.chat.postMessage({
+          channel: channelId,
+          text: message,
+      });
+      console.log(`Message sent to channel ${channelId}`);
+  } catch (error) {
+      console.error("Error sending message:", error);
+  }
 }
 
 const port = Number(process.env.PORT || 3000);
